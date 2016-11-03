83657e4950c543074e3efdfaaffc2d64c5ac7a71
Volodymyr Kytsun <vladimir.kytsun@gmail.com>
2016-11-03 12:00:17 +0200

    add maven project

---
+++ src/main/java/com/vkytsun/app/App.java
@@ -0,0 +1,13 @@
+package com.vkytsun.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
