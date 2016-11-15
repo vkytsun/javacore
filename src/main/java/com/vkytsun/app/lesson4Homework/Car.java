a5970826da08eee98b9df97cefcf80a5d7c778a8
Volodymyr Kytsun <vladimir.kytsun@gmail.com>
2016-11-15 02:16:11 +0200

    homework lesson4-carTest

--- src/main/java/com/vkytsun/app/lesson4Homework/Car.java
+++ src/main/java/com/vkytsun/app/lesson4Homework/Car.java
@@ -6,26 +6,26 @@ package com.vkytsun.app.lesson4Homework;
 public class Car {
     private int priceOfTheCar;
     private String modelOfTheСar;
-    public Car(int priceOfTheCar, String modelOfTheСar) {
-        this.priceOfTheCar = priceOfTheCar;
-        this.modelOfTheСar = modelOfTheСar;
+    public Car(int a, String Ford) {
+        priceOfTheCar = a;
+        modelOfTheСar = Ford;
     }
     public int getPriceOfTheCar() {
         return priceOfTheCar;
     }
-    public void setPriceOfTheCar(int priceOfTheCar) {
-        this.priceOfTheCar = priceOfTheCar;
+    public void setPriceOfTheCar(int a) {
+        priceOfTheCar = a;
     }
     public String getModelOfTheСar() {
         return modelOfTheСar;
     }
-    public void setModelOfTheСar(String modelOfTheСar) {
-        this.modelOfTheСar = modelOfTheСar;
+    public void setModelOfTheСar(String Ford) {
+        modelOfTheСar = Ford;
     }
-    public void buy(int discount) {
-        this.priceOfTheCar = priceOfTheCar - discount;
+    public void buy(int a, int discount) {
+        priceOfTheCar = a - discount;
     }
-    public void buyCredit(int percent) {
-        this.priceOfTheCar = priceOfTheCar + percent;
+    public void buyCredit(int a, int percent) {
+        priceOfTheCar = a + percent;
     }
 }
