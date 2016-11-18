4ffcdfa5baab6fef7f604ef62e9b3d5fd31bad33
Volodymyr Kytsun <vladimir.kytsun@gmail.com>
2016-11-18 18:01:02 +0200

    homework lesson5_v6

--- src/main/java/com/vkytsun/app/homework/lesson5/Menu.java
+++ src/main/java/com/vkytsun/app/homework/lesson5/Menu.java
@@ -2,49 +2,35 @@ package com.vkytsun.app.homework.lesson5;
 
 import java.util.Scanner;
 
-/**
- * Created by vkytsun on 11/16/16.
- */
+
 public class Menu {
     public static void menuRunner() {
-        System.out.println("Enter number of the operation which you want execute:");
+        int number;
+        do {
+        System.out.println("\nEnter number of the operation which you want execute:");
         System.out.println("1. Calculation circle area.");
         System.out.println("2. Comparison two circles area.");
         System.out.println("3. Calculation sides of right-angled triangle.");
         System.out.println("4. Define if number even or odd");
-        System.out.println("5. Exit");
+        System.out.println("0. Exit\n");
 
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
 
-        numberOperation(a);
+            if (a == 1) {
+                CircleRadius.calculate();
+            } else if (a == 2) {
+                    CircleArea.calculateBigger();
+                } else if (a == 3) {
+                        RightAngledTriangle.calculateTriangle();
+                    } else if (a == 4) {
+                            EvenOdd.defineEvenOdd();
+                        } else if (a == 0) {
+                                System.out.println("Cood bye!");
+                            } else
+                                System.out.println("incorrect number. Please, try again.");
+            number = a;
+        } while (number != 0);
     }
 
-    public static int numberOperation(int a) {
-
-        if (a == 1) {
-            CircleRadius.calculate();
-        } else {
-            if (a == 2) {
-                CircleArea.calculateBigger();
-            } else {
-                if (a == 3) {
-                    RightAngledTriangle.calculateTriangle();
-                } else {
-                    if (a == 4) {
-                        EvenOdd.defineEvenOdd();
-                    } else {
-                        if (a == 5) {
-                            System.out.println("Cood bye!");
-                        }
-                        else {
-                            System.out.println("incorrect number. Please, try again.");
-                        }
-                    }
-                }
-
-            }
-        }
-        return a;
-    }
 }
