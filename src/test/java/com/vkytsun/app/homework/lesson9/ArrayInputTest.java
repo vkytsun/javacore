c8c6102024957d654a61f0b094d7c36bea1b0faf
Volodymyr Kytsun <vladimir.kytsun@gmail.com>
2016-12-07 16:48:01 +0200

    test commit

--- src/test/java/com/vkytsun/app/homework/lesson9/ArrayInputTest.java
+++ src/test/java/com/vkytsun/app/homework/lesson9/ArrayInputTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class ArrayInputTest {
     @Test
     public void inputArray() throws Exception {
-
+        int[] expectedResult = {23,14,6,0,44,78,91,34,66,12};
 
     }
 
