102192b5b6edccc0c72a336cf584cfc472e52e23
Volodymyr Kytsun <vladimir.kytsun@gmail.com>
2016-12-17 18:15:58 +0200

    homework lesson13_v1

---
+++ src/main/java/com/vkytsun/runners/homework/Lesson13Runner.java
@@ -0,0 +1,12 @@
+package com.vkytsun.runners.homework;
+
+import com.vkytsun.app.homework.lesson13.Human;
+
+public class Lesson13Runner {
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.setAge(212);
+        System.out.println(human.getAge());
+
+    }
+}
