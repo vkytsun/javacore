77ffd1e4e18c87eaf31914685fb71de70b19f2f5
Volodymyr Kytsun <vladimir.kytsun@gmail.com>
2017-01-17 15:47:01 +0200

    add homework lesson16_v3

--- src/main/java/com/vkytsun/runners/homework/Lesson16Runner.java
+++ src/main/java/com/vkytsun/runners/homework/Lesson16Runner.java
@@ -15,47 +15,52 @@ import java.util.Scanner;
 жизни должен находится в enum.
 * */
 public class Lesson16Runner {
+    public static boolean[] randomIdRepeat;
     public static void main(String[] args) {
         String inputAge;
         String inputStatusOfLive;
-        String currentStar = "";
-        String nextStar = "";
-        int randomStar;
+        String randomStar;
+        int randomId;
         int guessed = 0;
         int attempts = 0;
         ListOfPeople[] celebrities = ListOfPeople.values();
         //System.out.println(Arrays.toString(celebrities));
+        randomIdRepeat = new boolean[celebrities.length];
+        for (int i = 0; i < randomIdRepeat.length; i++) {
+            randomIdRepeat[i] = false;
+        }
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-        do {
+        while (guessed < 3 && attempts < randomIdRepeat.length) {
             try {
                 attempts++;
-                if (nextStar.equals(currentStar)) {
-                    randomStar = random.nextInt(celebrities.length);
-                    nextStar = celebrities[randomStar].getFullName();
-                    System.out.println("How old is " + nextStar + "?");
+                randomId = random.nextInt(celebrities.length);
+                if (randomIdRepeat[randomId] == true) {
+                    random.nextInt(celebrities.length);
+                } else {
+                    randomIdRepeat[randomId] = true;
+                    randomStar = celebrities[randomId].getFullName();
+                    System.out.println("How old is " + randomStar + "?");
                     inputAge = scanner.nextLine();
                     //System.out.println(inputAge);
-                    if (Integer.parseInt(inputAge) == (celebrities[randomStar].getAge())) {
+                    if (Integer.parseInt(inputAge) == (celebrities[randomId].getAge())) {
                         System.out.println("Congratulations, you guessed age!");
                         System.out.println("This person is 'alive' or 'died'?");
                         inputStatusOfLive = scanner.nextLine();
-                        if (inputStatusOfLive.equalsIgnoreCase(celebrities[randomStar].getStatusOfLive())) {
+                        if (inputStatusOfLive.equalsIgnoreCase(celebrities[randomId].getStatusOfLive())) {
                             System.out.println("Congratulations, you guessed!");
                             guessed++;
                         } else {
                             System.out.println("Unfortunately, you did not guess.");
                         }
                     } else {
-                        System.out.println("You did not guess age! Age is " + celebrities[randomStar].getAge());
+                        System.out.println("You did not guess age! Age is " + celebrities[randomId].getAge());
                     }
-                    currentStar = nextStar;
                 }
             } catch(NumberFormatException e ){
                 System.out.println("You have entered incorrect format of age. Try again!");
             }
-        } while (guessed < 3 && attempts < celebrities.length);
-        if (guessed == 3) {
+        } if (guessed == 3) {
             System.out.println("Congratulations, you won!");
         } else {
             System.out.println("You used all attemts and lose. Try again!");
