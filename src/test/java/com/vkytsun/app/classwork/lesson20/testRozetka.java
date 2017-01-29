78e17c1cd5710f6c79d404d6bec7659ddb7463c6
vkytsun <vladimir.kytsun@gmail.com>
2017-01-29 21:58:30 +0200

    add classwork lesson20_v2

--- src/test/java/com/vkytsun/app/classwork/lesson20/testRozetka.java
+++ src/test/java/com/vkytsun/app/classwork/lesson20/testRozetka.java
@@ -2,44 +2,39 @@ package com.vkytsun.app.classwork.lesson20;
 
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.concurrent.TimeUnit;
 
 public class testRozetka {
 
     private WebDriver driver;
-    private String text = "Интернет магазин";
-    private String searchText = "Iphone 6s";
+    private String url = "http://rozetka.com.ua";
+    private String text = "Интернет магазин Rozetka.ua";
+    private String searchText = "Apple iPhone 6s";
 
     @Before
     public void setUp() throws Exception{
         driver = new FirefoxDriver();
         //WebDriverWait wait = new WebDriverWait(driver, 30);
-        driver.get("http://rozetka.com.ua");
+        driver.get(url);
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-
-
     }
     @Test
     public void testTitle() {
         driver.findElement(By.className("logo"));
         driver.findElement(By.xpath("//div[@class='logo']"));
-        //Assert.assertTrue(driver.getCurrentUrl().contains(text));
-        //Assert.assertTrue(driver.findElement(By.cssSelector("title")).getText().contains(text));
     }
-/*    @Test
+    @Test
     public void testSearchText() {
-        driver.findElement(By.xpath())
-    }*/
+        //driver.findElement(By.name("text")).sendKeys(searchText);
+        driver.findElement(By.xpath(".//*[@id='rz-search']/form/div[1]/div[2]/input")).sendKeys(searchText);
+        driver.findElement(By.xpath("//span[@class='btn-link btn-link-green']")).click();
+        driver.findElement(By.xpath("//div[@class='g-i-tile-i-title clearfix']/a[@href='http://rozetka.com.ua/apple_iphone_6s_32gb_rose_gold/p12136258/']"));
+    }
     @After
     public void tearDown() {
         driver.quit();
